192126b6d83cb924316210f6db96061f8c1174dc
Mahtab (Mattie) Nejati <mahtab.nejati@gmail.com>
2023-06-01 10:49:12 -0400

    exceptions added

---
+++ utils/exceptions.py
@@ -0,0 +1,22 @@
+class DebugException(Exception):
+    pass
+
+
+class ConfigurationException(Exception):
+    pass
+
+
+class MissingRootException(Exception):
+    def __init__(self, root_type, file_name):
+        self.file_name = file_name
+        self.root_type = root_type
+        self.message = f"Root node of type {self.root_type} not found in file {self.file_name}. Please make sure your language support configurations are correct."
+        super().__init__(self.message)
+
+
+class MissingArgumentsException(Exception):
+    def __init__(self, command_id, location):
+        self.command_id = command_id
+        self.location = location
+        self.message = f"{self.command_id} command at {self.location} is missing expected arguments."
+        super().__init__(self.message)
