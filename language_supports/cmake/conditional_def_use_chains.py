eee9eba5517a28d3c93ef8055d55f7964b66b10f
Mahtab (Mattie) Nejati <mahtab.nejati@gmail.com>
2024-02-22 10:42:44 -0500

    Support detecting recursive callables

--- language_supports/cmake/conditional_def_use_chains.py
+++ language_supports/cmake/conditional_def_use_chains.py
@@ -662,6 +662,21 @@ class ConditionalDefUseChains(cm.ConditionalDefUseChains):
     def visit_endmacro_clause(self, node_data, *args, **kwargs):
         return
 
+    def is_recursive_call(self, callsite_node_data, *args, **kwargs):
+        name = self.ast.get_name(callsite_node_data)
+        ancestors = self.ast.get_ancestors(callsite_node_data)
+        callable_definition_ancestors = filter(
+            lambda ancestor: (
+                ancestor["type"] in ["function_definition", "macro_definition"]
+            ),
+            ancestors.values(),
+        )
+        same_name_callable_definition_ancestors = filter(
+            lambda ancestor: self.ast.get_name(ancestor) == name,
+            callable_definition_ancestors,
+        )
+        return len(list(same_name_callable_definition_ancestors)) > 0
+
     def process_callable_definition_location(
         self, node_data, callable_type, actor_point
     ):
@@ -694,7 +709,9 @@ class ConditionalDefUseChains(cm.ConditionalDefUseChains):
         return
 
     def process_callable_call_location(self, node_data, def_point, actor_point):
-        # TODO: Recursive call detection is removed for technical issues.
+        if self.is_recursive_call(node_data):
+            self.generic_visit(node_data, actor_point)
+            return
         # Calls have an impact on the reachability of the content.
         self.add_condition_to_reachability_stack(node_data, actor_point)
         if def_point.type == "FUNCTION":
