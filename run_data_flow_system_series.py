83f54b4b700cb46cb6f2ee8fdc1db91c3cce6652
Mahtab (Mattie) Nejati <mahtab.nejati@gmail.com>
2024-02-24 20:55:07 -0500

    Make analysis a subprocess for each commit to avoid memory leak

--- run_data_flow_system_series.py
+++ run_data_flow_system_series.py
@@ -1,3 +1,4 @@
+from multiprocessing import Process
 from pydriller import Repository
 from pydriller.git import Git
 from tqdm import tqdm
@@ -124,115 +125,121 @@ clear_existing_data()
 
 # Run tool on commits
 chronological_commit_order = 0
-for commit in tqdm(repo.traverse_commits()):
-    print(f"Commit in process: {commit.hash}")
-    if commit.hash in completed_commits:
-        print("Commit previously analyzed.")
+if __name__ == "__main__":
+    for commit in tqdm(repo.traverse_commits()):
+        print(f"Commit in process: {commit.hash}")
+        if commit.hash in completed_commits:
+            print("Commit previously analyzed.")
+            chronological_commit_order += 1
+            continue
+        # Commit-level attributes that show whether the commit
+        # has affected build/non-build files
+        has_build = False
+        has_nonbuild = None  # TODO: Disabled to keep script simple
+
+        # Start analysis of the commit
+        commit_start = datetime.now()
+
+        # EXCEPTION_HANDLING_GitPython
+        # Error handling for missing commits
+        # GitPython, and consequently PyDriller,
+        # do not handle this well.
+        # Although PyDriller offers the
+        # only_modifications_with_file_types option,
+        # using such filtering throws an error if the
+        # commit is missing as GitPython attempts an access
+        # to the commit when iterating over the commits.
+        # In practice, this does not provide any
+        # performance improvements either, as the
+        # traversal takes place by iterating over
+        # all the commits and just skipping the ones
+        # filtered based on user's specifications.
+        # This is why use of
+        # "FILTER_BUILDY_COMMITS_AT_INITIALIZATION": "NO" is recommended
+        # in the configurations.
+        try:
+            # This will throw an error if the commit is missing
+            commit.modified_files
+        except AttributeError:
+            # Clear existing code and gumtree outputs
+            clear_existing_data()
+            if not (commit.hash in EXCLUDED_COMMITS):
+                raise DebugException(
+                    f"Submodule commit {commit.hash} must be excluded from analysis."
+                )
+            continue
+        except ValueError:
+            # Clear existing code and gumtree outputs
+            clear_existing_data()
+            if not (commit.hash in EXCLUDED_COMMITS):
+                raise DebugException(
+                    f"Missing commit {commit.hash} must be excluded from analysis."
+                )
+            continue
+
+        # Iterate over the languages and file naming conventions
+        # supported by the build system
+        for LANGUAGE in LANGUAGES:
+            PATTERNS = PATTERN_SETS[LANGUAGE]
+
+            # Identify if the commit has build modifications
+            if any(map(lambda mf: file_is_target(mf, PATTERNS), commit.modified_files)):
+                has_build = True
+
+                # Save time by skipping
+                if commit.hash in EXCLUDED_COMMITS:
+                    to_remove = SAVE_PATH / "commits" / commit.hash
+                    if to_remove.exists():
+                        shutil.rmtree(to_remove)
+                    continue
+
+                analyzer = Process(
+                    target=analyze_commit,
+                    args=[
+                        REPOSITORY,
+                        repo,
+                        git_repo,
+                        BRANCH,
+                        commit,
+                        ENTRY_FILES,
+                        LANGUAGE,
+                        PATTERNS,
+                        ROOT_PATH,
+                        SAVE_PATH,
+                    ],
+                )
+                analyzer.start()
+                analyzer.join()
+                gc.collect()
+
+        # Don't log if excluded
+        if commit.hash in EXCLUDED_COMMITS:
+            continue
+
         chronological_commit_order += 1
-        continue
-    # Commit-level attributes that show whether the commit
-    # has affected build/non-build files
-    has_build = False
-    has_nonbuild = None  # TODO: Disabled to keep script simple
+        # Log all changes
+        commit_data_df = pd.DataFrame(
+            {
+                "commit_hash": [commit.hash],
+                "chronological_commit_order": [chronological_commit_order],
+                "commit_parents": [commit.parents],
+                "has_build": [has_build],
+                "has_nonbuild": [has_nonbuild],
+                "is_missing": [False],
+                "elapsed_time": [datetime.now() - commit_start],
+            }
+        )
+        commit_data_df.to_csv(
+            SAVE_PATH / "all_commits.csv", mode="a", header=False, index=False
+        )
 
-    # Start analysis of the commit
-    commit_start = datetime.now()
+    # Checkout to head once done.
+    # This is currently disabled in
+    # system_commit_mode/sustem_diff_mode.py/SystemDiff()
+    # for performance improvement.
+    git_repo.checkout(BRANCH)
 
-    # EXCEPTION_HANDLING_GitPython
-    # Error handling for missing commits
-    # GitPython, and consequently PyDriller,
-    # do not handle this well.
-    # Although PyDriller offers the
-    # only_modifications_with_file_types option,
-    # using such filtering throws an error if the
-    # commit is missing as GitPython attempts an access
-    # to the commit when iterating over the commits.
-    # In practice, this does not provide any
-    # performance improvements either, as the
-    # traversal takes place by iterating over
-    # all the commits and just skipping the ones
-    # filtered based on user's specifications.
-    # This is why use of
-    # "FILTER_BUILDY_COMMITS_AT_INITIALIZATION": "NO" is recommended
-    # in the configurations.
-    try:
-        # This will throw an error if the commit is missing
-        commit.modified_files
-    except AttributeError:
-        # Clear existing code and gumtree outputs
-        clear_existing_data()
-        if not (commit.hash in EXCLUDED_COMMITS):
-            raise DebugException(
-                f"Submodule commit {commit.hash} must be excluded from analysis."
-            )
-        continue
-    except ValueError:
-        # Clear existing code and gumtree outputs
-        clear_existing_data()
-        if not (commit.hash in EXCLUDED_COMMITS):
-            raise DebugException(
-                f"Missing commit {commit.hash} must be excluded from analysis."
-            )
-        continue
+    # Clear existing code and gumtree outputs
+    clear_existing_data()
 
-    # Iterate over the languages and file naming conventions
-    # supported by the build system
-    for LANGUAGE in LANGUAGES:
-        PATTERNS = PATTERN_SETS[LANGUAGE]
-
-        # Identify if the commit has build modifications
-        if any(map(lambda mf: file_is_target(mf, PATTERNS), commit.modified_files)):
-            has_build = True
-
-            # Save time by skipping
-            if commit.hash in EXCLUDED_COMMITS:
-                to_remove = SAVE_PATH / "commits" / commit.hash
-                if to_remove.exists():
-                    shutil.rmtree(to_remove)
-                continue
-
-            analyze_commit(
-                REPOSITORY,
-                repo,
-                git_repo,
-                BRANCH,
-                commit,
-                ENTRY_FILES,
-                LANGUAGE,
-                PATTERNS,
-                ROOT_PATH,
-                SAVE_PATH,
-            )
-            gc.collect()
-
-    # Don't log if excluded
-    if commit.hash in EXCLUDED_COMMITS:
-        continue
-
-    chronological_commit_order += 1
-    # Log all changes
-    commit_data_df = pd.DataFrame(
-        {
-            "commit_hash": [commit.hash],
-            "chronological_commit_order": [chronological_commit_order],
-            "commit_parents": [commit.parents],
-            "has_build": [has_build],
-            "has_nonbuild": [has_nonbuild],
-            "is_missing": [False],
-            "elapsed_time": [datetime.now() - commit_start],
-        }
-    )
-    commit_data_df.to_csv(
-        SAVE_PATH / "all_commits.csv", mode="a", header=False, index=False
-    )
-
-# Checkout to head once done.
-# This is currently disabled in
-# system_commit_mode/sustem_diff_mode.py/SystemDiff()
-# for performance improvement.
-git_repo.checkout(BRANCH)
-
-# Clear existing code and gumtree outputs
-clear_existing_data()
-
-print(f"Finished processing in {datetime.now()-all_commits_start}")
+    print(f"Finished processing in {datetime.now()-all_commits_start}")
